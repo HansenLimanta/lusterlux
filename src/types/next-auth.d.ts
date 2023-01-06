e1a702517cc2223a697426cc4766f1edf6f98bcc
Hansen Limanta <hansenlimanta@gmail.com>
2023-01-06 18:28:10 +0700

    fix the User type

--- src/types/next-auth.d.ts
+++ src/types/next-auth.d.ts
@@ -1,4 +1,4 @@
-import { type DefaultSession } from "next-auth";
+import { type DefaultSession, DefaultUser } from "next-auth";
 
 declare module "next-auth" {
   /**
@@ -7,6 +7,12 @@ declare module "next-auth" {
   interface Session {
     user?: {
       id: string;
+      role: string;
     } & DefaultSession["user"];
+  }
+
+  interface User extends DefaultUser {
+    id: string;
+    role: string;
   }
 }
