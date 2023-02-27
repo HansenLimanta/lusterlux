65aa4bd65a2b1a8dc11c5dd6dd7fb55f7ef67ced
HansenLimanta <hansenlimanta@gmail.com>
2023-02-27 12:14:18 +0700

    minor bug fix

--- src/components/TestimonyCard.tsx
+++ src/components/TestimonyCard.tsx
@@ -12,7 +12,8 @@ const TestimonyCard: FC = ({}) => {
           it. The curls it creates are so beautiful and natural-looking, and it
           heats up quickly so I can get started on my hair right away. Overall,
           I would highly recommend this curl iron to anyone looking to add some
-          beautiful curls to their hair. It's definitely worth the investment!
+          beautiful curls to their hair. It&apos;s definitely worth the
+          investment!
         </p>
         <button className="mt-4 rounded-lg border border-gray-300 bg-white px-5 py-2.5 text-sm font-medium text-gray-900 hover:bg-gray-100 focus:outline-none focus:ring-4 focus:ring-gray-200">
           Curl Iron Type 1, 32mm
